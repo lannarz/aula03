4bcce9c0c9885f8c6437d021986c839353f5405f
MatheusKiyota <matheus.kiyota@estudante.ifms.edu.br>
2024-10-29 10:43:07 -0300

    modificaçao

--- src/pages/ListarProdutos.jsx
+++ src/pages/ListarProdutos.jsx
@@ -1,4 +1,3 @@
-<<<<<<< Updated upstream
 import styles from '../../styles/listarProdutos.module.css';
 
 export default function ListarProdutos({ lista }) {
@@ -19,62 +18,3 @@ export default function ListarProdutos({ lista }) {
         </div>
     );
 }
-=======
-<<<<<<< HEAD
-import { useEffect, useState } from "react";
-
-export default function Home() {
-
-    const [lista, setLista] = useState([]);
-
-    useEffect(() => {
-
-        const receberListaProdutos = async () => {
-            try {
-                const resposta = await fetch('https://fakestoreapi.com/products');
-                const dados = await resposta.json();
-                setLista(dados);
-            } catch {
-                alert('Ocorreu um erro na comunicação com o servidor!');
-            }
-        }
-
-        receberListaProdutos();
-    }, []);
-
-    return (
-        <>
-            <h1>Lista de Produtos</h1>
-            <ul>
-                {lista.map(produto => (
-                    <li key={produto.id}>
-                        <h2>{produto.title}</h2>
-                        <p>{produto.description}</p>
-                        <p>Preço: ${produto.price}</p>
-                        <img src={produto.image} alt={produto.title} width={100} />
-=======
-import styles from '../../styles/listarProdutos.module.css'
-
-export default function ListarProdutos({lista}) {
-    return(
-        <>
-            <ul className={styles.bloco}>
-                {lista.map(produto => (
-                    <li className={styles.li} key={produto.id}>
-                        <h2 className={styles.titulo}> {produto.title}</h2>
-                        <p>{produto.descripition}</p>
-                        <p className={styles.valor}>Valor: R${produto.price}</p>
-                        <img className={styles.foto} src={produto.image} alt={produto.title} width={200}/>
->>>>>>> ab3477114f9597cb35c7ecf129e6036a4731ba92
-                    </li>
-                ))}
-            </ul>
-        </>
-<<<<<<< HEAD
-    );
-}
-=======
-    )
-}
->>>>>>> ab3477114f9597cb35c7ecf129e6036a4731ba92
->>>>>>> Stashed changes
