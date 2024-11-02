601155e2d3866a0fc2a46578728a68a565d0e284
Lannarz <lanna.rizzo@estudante.ifms.edu.br>
2024-11-02 15:12:21 -0300

    Lannão

--- src/pages/ListarProdutos.jsx
+++ src/pages/ListarProdutos.jsx
@@ -1,20 +1,18 @@
-import styles from '../../styles/listarProdutos.module.css';
+import styles from '../../styles/listarProdutos.module.css'
 
-export default function ListarProdutos({ lista }) {
-    return (
-        <div className={styles.container}>
+export default function ListarProdutos({lista}) {
+    return(
+        <>
             <ul className={styles.bloco}>
                 {lista.map(produto => (
                     <li className={styles.li} key={produto.id}>
-                        <img className={styles.foto} src={produto.image} alt={produto.title} />
-                        <div className={styles.content}>
-                            <h2 className={styles.titulo}>{produto.title}</h2>
-                            <p className={styles.valor}>R$ {produto.price}</p>
-                            <button className={styles.botao}>Obter</button>
-                        </div>
+                        <h2 className={styles.titulo}> {produto.title}</h2>
+                        <p>{produto.descripition}</p>
+                        <p className={styles.valor}>Valor: R${produto.price}</p>
+                        <img className={styles.foto} src={produto.image} alt={produto.title} width={200}/>
                     </li>
                 ))}
             </ul>
-        </div>
-    );
+        </>
+    )
 }
