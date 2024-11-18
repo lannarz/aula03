6d37b6c85ebaadd4a391fb49e74835421e5d80ae
lannarz <lanna.rizzo@estudante.ifms.edu.br>
2024-11-18 16:14:46 -0400

    filter

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -21,12 +21,53 @@ export default function Home() {
         receberListaProdutos();
     }, []);
 
+    const orderAZ = () => {
+        const listaOrdenada = [...lista].sort((a, b) => a.title.localeCompare(b.title));
+        setLista(listaOrdenada)
+    }
+
+    const orderZA = () => {
+        const listaOrdenada = [...lista].sort((a, b) => b.title.localeCompare(a.title));
+        setLista(listaOrdenada)
+    }
+
+    const crescente = () => {
+        const listaOrdenada = [...lista].sort((a, b) => a.price-b.price);
+        setLista(listaOrdenada)
+    }
+    
+    const decrescente = () => {
+        const listaOrdenada = [...lista].sort((a, b) => b.price-a.price);
+        setLista(listaOrdenada)
+    }
+    
+
+
 
     return (
         <>
-             
 
            <h1 className={styles.lp}>Produtos Disponíveis</h1>
+
+
+            <div className={styles.filtros}>
+            <button className={styles.az} onClick={() => orderAZ()}>
+                AZ
+            </button>
+
+            <button className={styles.za} onClick={() => orderZA()}>
+                ZA
+            </button>
+
+            <button className={styles.cre}onClick={() => crescente()}>
+                Preço crescente
+            </button>
+
+            <button className={styles.decre} onClick={() => decrescente()}>
+                Preço decrescente
+            </button>
+            </div>
+
            <ListarProdutos lista={lista}/>
         </>
     );
